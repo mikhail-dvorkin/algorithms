e3bd85c0140f1da5d319069393cd1dc144b0d887
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2018-04-24 23:20:21 +0300

    Add traverse for hyperbola

--- Useful.java
+++ Useful.java
@@ -32,10 +32,10 @@ public class Useful {
 		// Filled Rhombus
 		for (int x = Math.max(cx - r, 0), xxLast = Math.min(cx + r, xSize - 1); x <= xxLast; x++) {
 			for (int y = Math.max(cy - r + Math.abs(x - cx), 0), yyLast = Math.min(cy + r - Math.abs(x - cx), ySize - 1); y <= yyLast; y++) {
-				
+				System.out.println(x + " " + y);
 			}
 		}
-		
+
 		// Border Rhombus
 		for (int x = Math.max(cx - r, 0), xxLast = Math.min(cx + r, xSize - 1); x <= xxLast; x++) {
 			for (int u = -1; u <= 1; u += 2) {
@@ -43,15 +43,44 @@ public class Useful {
 				if (y == cy && u == 1 || y < 0 || y >= ySize) {
 					continue;
 				}
-				
+				System.out.println(x + " " + y);
 			}
 		}
-		
+
 		// Filled Circle
 		for (int x = Math.max(cx - r, 0), xxLast = Math.min(cx + r, xSize - 1); x <= xxLast; x++) {
 			int hy = (int) Math.floor(Math.sqrt(r * r - (x - cx) * (x - cx)));
 			for (int y = Math.max(cy - hy, 0), yyLast = Math.min(cy + hy, ySize - 1); y <= yyLast; y++) {
-				
+				System.out.println(x + " " + y);
+			}
+		}
+
+		// Hyperbola
+		for (int area = 2; area <= r * r; area++) {
+			for (int sidex = 1; sidex * sidex <= area; sidex++) {
+				if (area % sidex != 0) {
+					continue;
+				}
+				int sidey = area / sidex;
+				int dx = sidex - 1;
+				int dy = sidey - 1;
+				for (int dir = 0; dir < 8; dir++) {
+					int swap = dir & 1;
+					int sx = (dir & 2) - 1;
+					int sy = ((dir >> 1) & 2) - 1;
+					if (dx == 0 && sx != sy) {
+						continue;
+					}
+					if (dx == dy && swap == 1) {
+						continue;
+					}
+					int x = cx + sx * (swap * dx + (1 - swap) * dy);
+					int y = cy + sy * (swap * dy + (1 - swap) * dx);
+					if (x < 0 || y < 0 || x >= xSize || y >= ySize) {
+						continue;
+					}
+					System.out.println(x + " " + y);
+				}
 			}
 		}
 	}
