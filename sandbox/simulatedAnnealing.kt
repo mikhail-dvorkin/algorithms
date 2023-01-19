cdebcfbd3f2528a0b2bcbc53bf688c65c1e608aa
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-01-19 19:05:41 +0300

    Add link about simulated annealing

--- sandbox/simulatedAnnealing.kt
+++ sandbox/simulatedAnnealing.kt
@@ -3,4 +3,5 @@
  * https://www.math.spbu.ru/user/gran/sb1/lopatin.pdf
  * https://web.archive.org/web/20110206102841/http://rain.ifmo.ru/cat/data/theory/unsorted/ai-annealing-2008/article.pdf
  * https://twitter.com/FakePsyho/status/1605570944537280512
+ * https://bminaiev.github.io/simulated-annealing
  */
