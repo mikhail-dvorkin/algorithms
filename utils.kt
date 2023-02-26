8d02d403523b25ab222dbbe1f1841747adb4bb14
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-02-26 14:18:36 +0300

    Extract modular arithmetics to separate file

--- utils.kt
+++ utils.kt
@@ -46,32 +46,6 @@ private fun IntRange.binarySearch(predicate: (Int) -> Boolean): Int {
 	return high // first true
 }
 
-//typealias Modular = Double; fun Number.toModular() = toDouble(); fun Number.toModularUnsafe() = toDouble(); typealias ModularArray = DoubleArray
-@JvmInline
-@Suppress("NOTHING_TO_INLINE")
-private value class Modular(val x: Int) {
-	companion object {
-		const val M = 998244353; val MOD_BIG_INTEGER = M.toBigInteger()
-	}
-	inline operator fun plus(that: Modular) = Modular((x + that.x).let { if (it >= M) it - M else it })
-	inline operator fun minus(that: Modular) = Modular((x - that.x).let { if (it < 0) it + M else it })
-	inline operator fun times(that: Modular) = Modular((x.toLong() * that.x % M).toInt())
-	inline operator fun div(that: Modular) = times(that.inverse())
-	inline fun inverse() = Modular(x.toBigInteger().modInverse(MOD_BIG_INTEGER).toInt())
-	override fun toString() = x.toString()
-}
-private fun Int.toModularUnsafe() = Modular(this)
-private fun Int.toModular() = Modular(if (this >= 0) { if (this < Modular.M) this else this % Modular.M } else { Modular.M - 1 - inv() % Modular.M })
-private fun Long.toModular() = Modular((if (this >= 0) { if (this < Modular.M) this else this % Modular.M } else { Modular.M - 1 - inv() % Modular.M }).toInt())
-private fun java.math.BigInteger.toModular() = Modular(mod(Modular.MOD_BIG_INTEGER).toInt())
-private fun String.toModular() = Modular(fold(0L) { acc, c -> (c - '0' + 10 * acc) % Modular.M }.toInt())
-@JvmInline
-private value class ModularArray(val data: IntArray) {
-	operator fun get(index: Int) = data[index].toModularUnsafe()
-	operator fun set(index: Int, value: Modular) { data[index] = value.x }
-}
-private inline fun ModularArray(n: Int, init: (Int) -> Modular) = ModularArray(IntArray(n) { init(it).x })
-
 private val isOnlineJudge = System.getProperty("ONLINE_JUDGE") == "true"
 private val stdStreams = (true to true).apply  { if (!isOnlineJudge) {
 	if (!first) System.setIn(java.io.File("input.txt").inputStream())
