2bf046382be2edb2b391dc036114283f022ec1c2
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-08-18 12:16:01 +0300

    Add erf (error function) calculation

--- ProbabilityTheory.java
+++ ProbabilityTheory.java
@@ -9,4 +9,15 @@ public class ProbabilityTheory {
 		double qp = (1 - p) / p;
 		return (Math.pow(qp, down) - 1) / (Math.pow(qp, up + down) - 1);
 	}
+
+	static double erf(double x) {
+		double t = 1.0 / (1 + 0.47047 * Math.abs(x));
+		double p = t * (0.3480242 + t * (-0.0958798 + t * 0.7478556));
+		double ans = 1 - p * Math.exp(-x * x);
+		return x >= 0 ? ans : -ans;
+	}
+
+	static double Phi(double x) {
+		return 0.5 * (1 + erf(x / Math.sqrt(2.0)));
+	}
 }
