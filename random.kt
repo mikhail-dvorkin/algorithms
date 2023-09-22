ac3e711e246ce906b3b9ab058d752182d9efc421
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2023-09-22 13:38:55 +0300

    Rename to nextPermutationSequence()

--- random.kt
+++ random.kt
@@ -1,16 +1,16 @@
 import kotlin.random.Random
 
-fun Random.nextSequence(n: Int) = sequence {
+fun Random.nextPermutationSequence(n: Int) = sequence {
 	val threshold = n / 2
 	val used = mutableSetOf<Int>()
 	while (used.size < threshold) {
 		val x = nextInt(n)
 		if (used.add(x)) yield(x)
 	}
-	yieldAll((0 until n).filter { it !in used }.shuffled(this@nextSequence))
+	yieldAll((0 until n).filter { it !in used }.shuffled(this@nextPermutationSequence))
 }
 
-fun <E> List<E>.asShuffledSequence(random: Random) = random.nextSequence(size).map { this[it] }
+fun <E> List<E>.asShuffledSequence(random: Random) = random.nextPermutationSequence(size).map { this[it] }
 
 fun <E> List<E>.randomOrNull(random: Random, predicate: (E) -> Boolean) =
 	asShuffledSequence(random).firstOrNull(predicate)
