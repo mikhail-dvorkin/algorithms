c9d12743843f1e97f7cd657652865ed677222271
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2024-08-31 12:10:22 +0300

    Implement IncrementalIncreasingSequence

---
+++ dataStructures.kt
@@ -0,0 +1,21 @@
+﻿data class IncrementalIncreasingSequence(val map: TreeMap<Int, Int>) {
+	constructor() : this(TreeMap())
+
+	init {
+		map[Int.MIN_VALUE] = Int.MIN_VALUE
+		map[Int.MAX_VALUE] = Int.MAX_VALUE
+	}
+
+	fun size() = map.size - 2
+
+	fun add(key: Int, value: Int) {
+		val floorEntry = map.floorEntry(key)
+		if (floorEntry.value >= value) return
+		map[key] = value
+		while (true) {
+			val entry = map.higherEntry(key)
+			if (entry.value > value) break
+			map.remove(entry.key)
+		}
+	}
+}
